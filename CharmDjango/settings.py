6725453e36b46503e3b8a8ffa2cfcd6e10b79695
mk <twelvegold@gmail.com>
2022-03-18 10:01:13 +0900

    djhe

--- CharmDjango/settings.py
+++ CharmDjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_heroku
+
 from pathlib import Path
 import os
 
@@ -166,5 +166,5 @@ X_FRAME_OPTOPNS = 'SAMEORIGIN'
 
 
 
-
+import django_heroku
 django_heroku.settings(locals())
