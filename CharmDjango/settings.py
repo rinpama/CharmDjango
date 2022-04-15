2a6000561699d2c5968dbef495e3f3a4b9a7819f
mk <twelvegold@gmail.com>
2022-04-16 08:51:41 +0900

    re6

--- CharmDjango/settings.py
+++ CharmDjango/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)    #スタティック�
 
 # 本番環境 # 例 # STATIC_ROOT='/var/www/static'
 STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
