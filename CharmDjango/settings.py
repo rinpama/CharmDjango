a420ff0836a34e5cc27f1516fa223d8621a7cb2c
mk <twelvegold@gmail.com>
2022-04-20 08:52:46 +0900

    staticurl

--- CharmDjango/settings.py
+++ CharmDjango/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = '/static/'     #スタティックファイルの URL を指定します。
+STATIC_URL = '/static/'     #スタティックファイルの URL を指定します。
 # ## 開発時 #
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)    #スタティックファイルが格納されるディレクトリを指定します。
 
@@ -163,7 +163,7 @@ STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
