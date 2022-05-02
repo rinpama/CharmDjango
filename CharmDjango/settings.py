e9e6c97b1c16978b2d71a156ba0ad572fce45a8d
mk <twelvegold@gmail.com>
2022-05-02 15:12:01 +0900

    static

--- CharmDjango/settings.py
+++ CharmDjango/settings.py
@@ -163,7 +163,7 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
 AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = None
 STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATICFILES_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS_S3_REGION_NAME = 'ap-northeast-1'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
