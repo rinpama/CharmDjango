91bd5a6e48810ee9378a2cc0bc99011fcef9044e
mk <twelvegold@gmail.com>
2022-05-06 08:17:02 +0900

    dep

--- CharmDjango/settings.py
+++ CharmDjango/settings.py
@@ -166,8 +166,8 @@ STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS_S3_REGION_NAME = 'ap-northeast-1'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
